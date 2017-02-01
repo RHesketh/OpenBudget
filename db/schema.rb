07cb8f4d71f2eedbbbb9d5b0d1b9ab6f8c9ef5bd
Rob Hesketh <rhesketh.uk@gmail.com>
2017-02-01 22:33:52 +0000

    Added FactoryGirl for testing

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 20170201221929) do
 
   create_table "users", force: :cascade do |t|
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
